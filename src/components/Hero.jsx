1a5c17de590a41fa43a961ca93546456d519e61d
daitoch <farhanch101@outlook.com>
2022-10-24 19:57:11 +0500

    Hero Section Added with gradient and Get Started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { discount, robot } from "../assets";
+import GetStarted from "./Getstarted";
 import styles from "../style"
 
 const Hero = () => {
@@ -19,16 +20,29 @@ const Hero = () => {
                 <div className="flex flex-row justify-between items-center w-full">
                     <h1 className="flex-1 font-popping text-white font-semibold ss:text-[72px] ss:leading-[100px] leading-[75px] text-[52px]">
                         The Next <br className=" sm:block hidden"/>
-                        <span className="text-gradient">Generation</span> {' '} <br />
-                        Payment Method.
+                        <span className="text-gradient">Generation</span>
                     </h1>
 
-                    <div className="">
-
+                    <div className="ss:flex hidden md:mr-4 mr-0">
+                        <GetStarted />
                     </div>
 
                 </div>
 
+                <h1 className="font-popping text-white font-semibold ss:text-[62px] ss:leading-[100px] leading-[75px] text-[52px]">
+                    Payment Method
+                </h1>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                    Our team of experts uses methodology to identify the credit cards most likely to fit your needs. We examine annual precentage rates, annual fees.
+                </p>
+
+            </div>
+            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+                <img src={robot} alt="Robotic arm" className="w-[100%] h-[100%] relative z-[5]" />
+
+                <div className="absolute z-[0] pink__gradient w-[40%] h-[35%] top-0"/>
+                <div className="absolute z-[1] white__gradient w-[80%] h-[50%] bottom-40"/>
+                <div className="absolute z-[0] blue__gradient w-[50%] h-[50%] bottom-20"/>
             </div>
         </section>
     )
